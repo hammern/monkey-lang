5b617ec5bfce3978d648ecf69db60f4684622919
Nadav Hammer <twindata36@gmail.com>
2024-04-26 17:56:23 +0300

    feat(chapter 3.5): evaluating prefix expressions

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -1,4 +1,4 @@
-use crate::parser::ast::{Expression, Literal, Statement, Statements};
+use crate::parser::ast::{Expression, Literal, Prefix, Statement, Statements};
 
 use self::object::Object;
 
@@ -19,11 +19,41 @@ pub fn eval(statements: Statements) -> Option<Object> {
 
 fn eval_expression(expression: Expression) -> Option<Object> {
     match expression {
-        Expression::Literal(literal) => match literal {
-            Literal::Int(int) => Some(Object::Int(int)),
-            Literal::Bool(bool) => Some(Object::Bool(bool)),
-        },
+        Expression::Literal(literal) => Some(eval_literal_expression(literal)),
+        Expression::Prefix(prefix, right) => {
+            Some(eval_prefix_expression(prefix, eval_expression(*right)?))
+        }
         _ => None,
+    }
+}
+
+fn eval_literal_expression(literal: Literal) -> Object {
+    match literal {
+        Literal::Int(int) => Object::Int(int),
+        Literal::Bool(bool) => Object::Bool(bool),
+    }
+}
+
+fn eval_prefix_expression(operator: Prefix, right: Object) -> Object {
+    match operator {
+        Prefix::Bang => eval_bang_operator_expression(right),
+        Prefix::Minus => eval_minus_prefix_operator_expression(right),
+    }
+}
+
+fn eval_bang_operator_expression(right: Object) -> Object {
+    match right {
+        Object::Bool(true) => Object::Bool(false),
+        Object::Bool(false) => Object::Bool(true),
+        Object::Null => Object::Bool(true),
+        _ => Object::Bool(false),
+    }
+}
+
+fn eval_minus_prefix_operator_expression(right: Object) -> Object {
+    match right {
+        Object::Int(int) => Object::Int(-int),
+        _ => Object::Null,
     }
 }
 
@@ -44,7 +74,12 @@ mod test {
 
     #[test]
     fn test_eval_integer_expression() {
-        let tests = vec![("5", Object::Int(5)), ("10", Object::Int(10))];
+        let tests = vec![
+            ("5", Object::Int(5)),
+            ("10", Object::Int(10)),
+            ("-5", Object::Int(-5)),
+            ("-10", Object::Int(-10)),
+        ];
 
         test_eval(tests);
     }
@@ -52,6 +87,20 @@ mod test {
     #[test]
     fn test_eval_boolean_expression() {
         let tests = vec![("true", Object::Bool(true)), ("false", Object::Bool(false))];
+
+        test_eval(tests);
+    }
+
+    #[test]
+    fn test_eval_bang_operator() {
+        let tests = vec![
+            ("!true", Object::Bool(false)),
+            ("!false", Object::Bool(true)),
+            ("!5", Object::Bool(false)),
+            ("!!true", Object::Bool(true)),
+            ("!!false", Object::Bool(false)),
+            ("!!5", Object::Bool(true)),
+        ];
 
         test_eval(tests);
     }
